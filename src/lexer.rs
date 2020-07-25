cc0186b5be9872e45f60228ffb25b80fb5d69bde
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-07-26 02:22:06 +0900

    step3: introduce tokenizer :(

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,6 +4,8 @@ use crate::strtol;
 use std::str::{Chars};
 use std::iter::{Peekable};
 
+// 本当はimpl Iter<Item=Token>を返したい
+// pub fn tokenize(chars: &mut Peekable<Chars>) -> impl Iter<Item=Token>
 pub fn tokenize(chars: &mut Peekable<Chars>) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
 
@@ -24,6 +26,7 @@ pub fn tokenize(chars: &mut Peekable<Chars>) -> Vec<Token> {
                 chars.next();
             },
             '0'..='9' => {
+                // strtolで既に数字の次まで進んでいるのでchars.next()はしない
                 let num = strtol::<usize>(chars).expect("数字ではありません");
                 let token = Token::Num{
                     val: num,
@@ -31,20 +34,22 @@ pub fn tokenize(chars: &mut Peekable<Chars>) -> Vec<Token> {
                 };
 
                 tokens.push(token);
-                chars.next();
             }
             _ => {
+                // 本当はエラーにするべき
+                chars.next();
                 continue;
             }
         };
-    }
+    };
+    tokens.push(Token::Eof);
 
     tokens
 }
 
 #[test]
 fn tokenize_test() {
-    let input = &mut "1 + 2 + 3 -2".chars().peekable();
+    let input = &mut "' 1 + 2 + 3 -2 '".chars().peekable();
     let result = tokenize(input);
 
     println!("{:?}", result);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate rust_chibicc;
-use rust_chibicc::{strtol};
 use rust_chibicc::lexer::tokenize;
+use rust_chibicc::token::Token;
 use std::env;
 
 fn main() {
@@ -11,36 +11,62 @@ fn main() {
     }
 
 
-    let chars = &mut args.get(1)
-        .expect("第一引数が取得できませんでした")
-        .chars()
-        .peekable();
+    let arg1 = args.get(1)
+        .expect("第一引数が取得できませんでした");
 
-    let _token = tokenize(chars);
+    // https://stackoverflow.com/questions/54056268/temporary-value-is-freed-at-the-end-of-this-statement
+    // tokenize(&mut arg1.chars().peekable()) <- Vec<Token>
+    //     .iter() <- Iter<'_, Token>
+    //     .peekable() <- Peekable<Token>
+    // Vec<Token>がfreeされるので，一旦変数束縛しないといけない
+    // https://doc.rust-jp.rs/book/second-edition/ch04-02-references-and-borrowing.html#a%E5%AE%99%E3%81%AB%E6%B5%AE%E3%81%84%E3%81%9F%E5%8F%82%E7%85%A7
+    // let tokens = tokenize(&mut arg1.chars().peekable()).iter().peekable();
+    let tokens = tokenize(&mut arg1.chars().peekable());
+    let mut peekable_tokens = tokens.iter().peekable();
+
+    println!("{:?}", tokens);
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
 
-    let first_num = strtol::<usize>(chars).expect("最初のトークンが数字ではないです");
-    println!("  mov rax, {}", first_num);
+    if let Some(Token::Num { val, .. }) = peekable_tokens.peek() {
+        println!("  mov rax, {}", *val);
+        peekable_tokens.next();
+    }
 
-    while let Some(ch) = chars.next() {
-        match ch {
-            '+' => {
-                let num = strtol::<usize>(chars).expect("parseできませんでした");
-                println!("  add rax, {}", num);
-            },
-            '-' => {
-                let num = strtol::<usize>(chars).expect("parseできませんでした");
-                println!("  sub rax, {}", num);
+    while let Some(tk) = peekable_tokens.peek() {
+        match tk {
+            Token::Reserved{ op, ..} => {
+                peekable_tokens.next();
+                if *op == '+' {
+                    match peekable_tokens.next().unwrap() {
+                        Token::Num { val, .. } => {
+                            println!("  add rax, {}", *val);
+                        },
+                        tk @ _ => {
+                            eprintln!("数字ではありません: {:?}", tk);
+                            return
+                        }
+                    };
+                } else if *op == '-' {
+                    match peekable_tokens.next().unwrap() {
+                        Token::Num { val, .. } => {
+                            println!("  add rax, {}", *val);
+                        },
+                        tk @ _ => {
+                            eprintln!("数字ではありません: {:?}", tk);
+                            return
+                        }
+                    };
+                }
             },
             _ => {
-                eprintln!("予期しない文字です: {}", ch);
-                return
+                break;
             }
         }
     }
 
     println!("  ret");
+    return
 }
