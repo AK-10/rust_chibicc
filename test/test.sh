e0309f5503ff5980b27e5fa2d8559abb9552cac1
AK-10 <atsushi524k10@gmail.com>
2021-01-02 05:46:32 +0900

    wip: fix test

--- test/test.sh
+++ test/test.sh
@@ -92,6 +92,7 @@ assert 7 'int main() { return add2(3,4); } int add2(int x, int y) { return x+y; 
 assert 1 'int main() { return sub2(4,3); } int sub2(int x, int y) { return x-y; }'
 assert 55 'int main() { return fib(9); } int fib(int x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }'
 
+
 assert 3 'int main() { int x=3; return *&x; }'
 assert 3 'int main() { int x=3; int *y=&x; int **z=&y; return **z; }'
 
@@ -100,7 +101,8 @@ assert 3 'int main() { int x=3; int *y=&x; int **z=&y; return **z; }'
 assert 5 'int main() { int x=3; int y=5; return *(&x+1); }'
 assert 5 'int main() { int x=3; int y=5; return *(1+&x); }'
 assert 3 'int main() { int x=3; int y=5; return *(&y-1); }'
-assert 2 'int main() { int x=3; return *(&x+2)-&x; }'
+
+assert 2 'int main() { int x=3; return (&x+2)-&x; }'
 assert 5 'int main() { int x=3; int y=5; int *z=&x; return *(z+1); }'
 assert 3 'int main() { int x=3; int y=5; int *z=&y; return *(z-1); }'
 assert 5 'int main() { int x=3; int *y=&x; *y=5; return x; }'
