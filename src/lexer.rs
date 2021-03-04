c2e2dd5f09112e594d2356299841013fe3f76ad3
AK-10 <atsushi524k10@gmail.com>
2021-03-04 18:38:41 +0900

    wip

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 use crate::token::Token;
+use std::str::FromStr;
 
 // TODO: LexerErrorの定義
 // TODO: Location structがほしい
@@ -22,22 +23,88 @@ impl<'a> Tokenizer {
         let mut tokens = Vec::<Token>::new();
 
         while self.pos <= self.user_input.len() - 1 {
-            match self.user_input.get(self.pos) {
+            let c = self.current().expect("pos is out of user_input range");
+            match c {
+                // line comment
+                _ if self.multi_get(2) == ['/', '/'] => {
+                    while self.current().unwrap() != '\n' {
+                        self.pos += 1;
+                    }
+                },
                 // "=" or "=="
-                Some('=') => {
+                '=' => {
                     let token = self.tokenize_eq()?;
                     tokens.push(token);
-                }
+                },
                 // "!="
-                Some('!') => {
+                '!' => {
                     let token = self.tokenize_not()?;
                     tokens.push(token);
+                },
+                // "<" or "<="
+                '<' => {
+                    let token = self.tokenize_lt()?;
+                    tokens.push(token);
+                },
+                '>' => {
+                    let token = self.tokenize_gt()?;
+                    tokens.push(token);
                 }
-                _ => {
-                    break
+                // binary or unary oeprator
+                '+' | '-' | '*' | '&' | '/' => {
+                    self.pos += 1;
+                    let token = Token::Reserved { op: c.to_string() };
+                    tokens.push(token);
+                },
+                // symbol
+                '(' | ')' | ';' | '{' | '}' | ',' | '[' | ']' => {
+                    self.pos += 1;
+                    let token = Token::Symbol(c.to_string());
+                    tokens.push(token);
+                },
+                // string
+                '"' => {
+                    let contents = self.read_string_literal();
+                    match contents {
+                        Ok(c) => tokens.push(Token::Str(c)),
+                        Err(e) => {
+                            let msg = format!("error occured in tokenizing string: {}", e);
+                            return Err(msg)
+                        }
+                    }
+                },
+                // num
+                '0' ..= '9' => {
+                    let num = self.strtol::<isize>()?;
+                    let token = Token::Num {
+                        val: num,
+                        t_str: num.to_string()
+                    };
+
+                    tokens.push(token);
+                },
+                ws if ws.is_whitespace() => {
+                    self.pos += 1;
+                    continue
+                },
+                // ident or reserved
+                'a' ..= 'z' | 'A' ..= 'Z' => {
+                    let letter = self.get_letter();
+                    if KEYWORDS.contains(&&*letter) {
+                        tokens.push(Token::Reserved{ op: letter });
+                    } else {
+                        tokens.push(Token::Ident { name: letter })
+                    }
+                },
+                unsupported => {
+                    // unsuupported character
+                    let msg = format!("unsupported character: {}", unsupported);
+                    return Err(msg)
                 }
             }
         }
+
+        tokens.push(Token::Eof);
 
         Ok(tokens)
     }
@@ -46,18 +113,19 @@ impl<'a> Tokenizer {
         &self.user_input[self.pos ..= (self.pos + n)]
     }
 
+    fn current(&self) -> Option<char> {
+        self.user_input.get(self.pos).map(|c| *c)
+    }
+
     fn tokenize_eq(&mut self) -> Result<Token, String> {
         self.pos += 1;
-
         match self.user_input.get(self.pos) {
             Some('=') => {
                 self.pos += 1;
                 Ok(Token::Reserved { op: "==".to_string() })
-            }
-            Some(_) => {
-                Ok(Token::Reserved { op: "=".to_string() })
-            }
-            _ => Err("must exist after =".to_string())
+            },
+            Some(_) => Ok(Token::Reserved { op: "=".to_string() }),
+            _ => Err("token must exist after =".to_string())
         }
     }
 
@@ -66,9 +134,118 @@ impl<'a> Tokenizer {
             Some('=') => {
                 self.pos += 2;
                 Ok(Token::Reserved { op: "!=".to_string() })
-            }
-            _ => Err("must exist after !".to_string())
+            },
+            _ => Err("token must exist after !".to_string())
         }
+    }
+
+    fn tokenize_lt(&mut self) -> Result<Token, String> {
+        self.pos += 1;
+        match self.user_input.get(self.pos) {
+            Some('=') => {
+                self.pos += 1;
+                Ok(Token::Reserved { op: "<=".to_string() })
+            },
+            Some(_) => Ok(Token::Reserved { op: "<".to_string() }),
+            _ => Err("token must exist after >".to_string())
+        }
+    }
+
+    fn tokenize_gt(&mut self) -> Result<Token, String> {
+        self.pos += 1;
+        match self.user_input.get(self.pos) {
+            Some('=') => {
+                self.pos += 1;
+                Ok(Token::Reserved { op: ">=".to_string() })
+            },
+            Some(_) => Ok(Token::Reserved { op: ">".to_string() }),
+            _ => Err("token must exist after >".to_string())
+        }
+    }
+
+    fn read_string_literal(&mut self) -> Result<Vec<u8>, String> {
+        // skip first '"'
+        self.pos += 1;
+
+        let mut str_content = Vec::<u8>::new();
+
+        while let Some(c) = self.current() {
+            match c {
+                // insert null char at last
+                '"' => {
+                    self.pos += 1;
+                    // push '\0'
+                    str_content.push(0);
+                    break
+                },
+                // escaped
+                '\\' => {
+                    self.pos += 1;
+                    str_content.push(self.read_escaped_literal());
+                },
+                _ => {
+                    self.pos += 1;
+                    str_content.push(c as u8);
+                }
+
+            }
+        }
+
+        Ok(str_content)
+    }
+
+    fn read_escaped_literal(&mut self) -> u8 {
+        let escaped = self.current().map(|c| {
+            match c {
+                'a' => 7,
+                'b' => 8,
+                't' => 9,
+                'n' => 10,
+                'v' => 11,
+                'f' => 12,
+                'r' => 13,
+                'e' => 27,
+                '0' => 0,
+                _ => c as u8 // 「escapeできない文字だよ」でもいい説ある
+            }
+        });
+
+        self.pos += 1;
+
+        escaped.expect("failed read_escaped_literal")
+    }
+
+    fn strtol<T: FromStr>(&mut self) -> Result<T, String> {
+        let mut num_str = String::new();
+        while let Some(c) = self.current() {
+            match c {
+                num if num.is_digit(10) => {
+                    self.pos += 1;
+                    num_str.push(num);
+                },
+                _ => break
+            }
+        }
+
+        num_str.parse::<T>()
+            .or(Err("failed pasring num".to_string()))
+    }
+
+    fn get_letter(&mut self) -> String {
+        let mut letter = String::new();
+        while let Some(c) = self.current() {
+            match c {
+                // '0' ..= '9'は呼び出し側で排除されているので，ここでは含めて良い(文字列の先頭以外は含めて良い)
+                'a' ..= 'z' | 'A' ..= 'Z' | '0' ..= '9' | '|' => {
+                    self.pos += 1;
+                    letter.push(c);
+                },
+                _ => break
+
+            }
+        }
+
+        letter
     }
 }
 
@@ -256,23 +433,7 @@ impl<'a> Tokenizer {
 //     Ok(str_content)
 // }
 // 
-// fn read_escaped_literal(chars: &mut Peekable<Enumerate<Chars>>) -> u8 {
-//     chars.next().map(|(_, ch)| {
-//         match ch {
-//             'a' => 7,
-//             'b' => 8,
-//             't' => 9,
-//             'n' => 10,
-//             'v' => 11,
-//             'f' => 12,
-//             'r' => 13,
-//             'e' => 27,
-//             '0' => 0,
-//             _ => ch as u8 // 「escapeできない文字だよ」でもいい説ある
-//         }
-//     }).expect("failed read_escaped_literal")
-// }
-// 
+//
 // fn tokenize_eq(chars_with_index: &mut Peekable<Enumerate<Chars>>) -> Token {
 //     match chars_with_index.peek() {
 //         Some((_, '=')) => {
